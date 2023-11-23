71c94d29a484e7fbbdd2d5394934141ea6a16e4c
Fabio Mendes <fabiomendesdev@outlook.com>
2023-11-23 16:13:44 -0300

    TimeZone

---
+++ TimeZone/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TimeZone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var utcDate = DateTime.UtcNow;
+
+            var timezones = TimeZoneInfo.GetSystemTimeZones();
+            foreach(var timezone in timezones )
+            {
+                Console.WriteLine(timezone);
+                Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(utcDate, timezone));
+                Console.WriteLine("-------");
+            }
+        }
+    }
+}
